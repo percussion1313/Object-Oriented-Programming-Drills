ee1fe18e4f0ca2caee70dd104c1a41d64f76fbe3
percussion1313 <peterjacobgrant@gmail.com>
2018-06-06 10:12:18 -0500

    Added 5 seperate people and called function

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('Testing')
+
+//Added 5 people
+var p1 = {
+    name: 'Steve',
+    sayHello: function () {
+        console.log(`Hello! My name is ${p1.name}`);
+    }   
+}
+p1.sayHello();
+
+var p2 = {
+    name: 'Julian',    
+    sayHello: function () {
+        console.log(`Hello! My name is ${p2.name}`);
+    }
+}
+
+p2.sayHello();
+var p3 = {
+    name: 'Jordan',
+    sayHello: function () {
+        console.log(`Hello! My name is ${p3.name}`);
+    }
+}
+
+p3.sayHello();
+var p4 = {
+    name: 'Scott',
+    sayHello: function () {
+        console.log(`Hello! My name is ${p4.name}`);
+    }
+}
+
+p4.sayHello();
+var p5 = {
+name: 'Cheryl',
+    sayHello: function () {
+        console.log(`Hello! My name is ${p5.name}`);
+    }
+}
+
+p5.sayHello();  
+
